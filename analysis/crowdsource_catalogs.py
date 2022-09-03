882d208baa0e33394952cd7c786060c83357b7fe
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-09-03 17:07:24 -0400

    add crowdsourcer

---
+++ analysis/crowdsource_catalogs.py
@@ -0,0 +1,170 @@
+import crowdsource
+import regions
+import numpy as np
+from astropy.convolution import convolve, Gaussian2DKernel
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy.visualization import simple_norm
+from astropy import wcs
+from astropy import table
+from astropy import units as u
+from astropy.io import fits
+
+from crowdsource import crowdsource_base
+from crowdsource.crowdsource_base import fit_im, psfmod
+
+from astroquery.svo_fps import SvoFps
+
+import pylab as pl
+pl.rcParams['figure.facecolor'] = 'w'
+pl.rcParams['image.origin'] = 'lower'
+
+import os
+os.environ['WEBBPSF_PATH'] = '/orange/adamginsburg/jwst/webbpsf-data/'
+with open(os.path.expanduser('~/.mast_api_token'), 'r') as fh:
+    os.environ['MAST_API_TOKEN'] = fh.read().strip()
+import webbpsf
+
+
+basepath = '/orange/adamginsburg/jwst/brick/'
+
+for filtername in ('F405N', 'F410M', 'F466N'):
+    for module in ('nrca', 'nrcb'):
+        try:
+            pupil = 'clear'
+            fh = fits.open(f'{basepath}/{filtername}/pipeline/jw02221-o001_t001_nircam_{pupil}-{filtername.lower()}-{module}_i2d.fits')
+        except Exception:
+            pupil = 'F444W'
+            fh = fits.open(f'{basepath}/{filtername}/pipeline/jw02221-o001_t001_nircam_{pupil}-{filtername.lower()}-{module}_i2d.fits')
+
+        im1 = fh
+        data = im1[1].data
+        instrument = im1[0].header['INSTRUME']
+        telescope = im1[0].header['TELESCOP']
+        filt = im1[0].header['FILTER']
+
+        wavelength_table = SvoFps.get_transmission_data(f'{telescope}/{instrument}.{filt}')
+        obsdate = im1[0].header['DATE-OBS']
+
+        nrc = webbpsf.NIRCam()
+        nrc.load_wss_opd_by_date(f'{obsdate}T00:00:00')
+        nrc.filter = filt
+        nrc.detector = f'{module.upper()}5' # I think NRCA5 must be the "long" detector?
+        grid = nrc.psf_grid(num_psfs=16, all_detectors=False)
+
+        yy, xx = np.indices([31,31], dtype=float)
+        grid.x_0 = grid.y_0 = 15.5
+        psf_model = crowdsource.psf.SimplePSF(stamp=grid(xx,yy))
+
+        ww = wcs.WCS(im1[1].header)
+        cen = ww.pixel_to_world(im1[1].shape[1]/2, im1[1].shape[0]/2) 
+        reg = regions.RectangleSkyRegion(center=cen, width=1.5*u.arcmin, height=1.5*u.arcmin)
+        preg = reg.to_pixel(ww)
+        mask = preg.to_mask()
+        cutout = mask.cutout(im1[1].data)
+        errcutout = mask.cutout(im1[2].data)
+
+        weight = errcutout**-2
+        weight[errcutout < 1e-5] = 0
+        weight[errcutout == 0] = np.nanmedian(weight)
+
+        maxweight = np.nanpercentile(weight, 99)
+        minweight = np.nanpercentile(weight, 1)
+        weight[weight > maxweight] = maxweight
+        weight[weight < minweight] = minweight
+
+
+        results_unweighted  = fit_im(data, psf_model, weight=np.ones_like(data)*np.nanmedian(weight),
+                                        #psfderiv=np.gradient(-psf_initial[0].data),
+                                        nskyx=1, nskyy=1, refit_psf=False, verbose=True)
+        stars, modsky, skymsky, psf = results_unweighted
+
+        fits.BinTableHDU(data=stars).writeto(f"{basepath}/{filtername}/{filtername.lower()}_{module}_crowdsource_unweighted.fits", overwrite=True)
+        fits.PrimaryHDU(data=skymsky, header=im1[1].header).writeto(f"{basepath}/{filtername}/{filtername.lower()}_{module}_crowdsource_skymodel_unweighted.fits")
+
+
+        pl.figure(figsize=(12,12))
+        pl.subplot(2,2,1).imshow(data, norm=simple_norm(data, stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("Data")
+        pl.subplot(2,2,2).imshow(modsky, norm=simple_norm(modsky, stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im model+sky")
+        pl.subplot(2,2,3).imshow(skymsky, norm=simple_norm(skymsky, stretch='asinh'), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im sky+skym")
+        pl.subplot(2,2,4).imshow(data, norm=simple_norm(data, stretch='log', max_percent=99.95), cmap='gray')
+        pl.subplot(2,2,4).scatter(stars['y'], stars['x'], marker='x', color='r', s=5, linewidth=0.5)
+        pl.xticks([]); pl.yticks([]); pl.title("Data with stars");
+        pl.savefig(f'{basepath}/{filtername}/pipeline/jw02221-o001_t001_nircam_F444W-{filtername.lower()}-{module}_catalog_diagnostics_unweighted.png',
+                   bbox_inches='tight')
+
+        pl.figure(figsize=(12,12))
+        pl.subplot(2,2,1).imshow(data[:128,:128], norm=simple_norm(data[:128,:128], stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("Data")
+        pl.subplot(2,2,2).imshow(modsky[:128,:128], norm=simple_norm(modsky[:128,:128], stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im model+sky")
+        pl.subplot(2,2,3).imshow(skymsky[:128,:128], norm=simple_norm(skymsky[:128,:128], stretch='asinh'), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im sky+skym")
+        pl.subplot(2,2,4).imshow(data[:128,:128], norm=simple_norm(data[:128,:128], stretch='log', max_percent=99.95), cmap='gray')
+        pl.subplot(2,2,4).scatter(stars['y'], stars['x'], marker='x', color='r', s=8, linewidth=0.5)
+        pl.axis([0,128,0,128])
+        pl.xticks([]); pl.yticks([]); pl.title("Data with stars");
+        pl.savefig(f'{basepath}/{filtername}/pipeline/jw02221-o001_t001_nircam_F444W-{filtername.lower()}-{module}_catalog_diagnostics_zoom_unweighted.png',
+                   bbox_inches='tight')
+
+        # pl.figure(figsize=(10,5))
+        # pl.subplot(1,2,1).imshow(psf_model(30,30), norm=simple_norm(psf_model(30,30), stretch='log'), cmap='cividis')
+        # pl.title("Input model")
+        # pl.subplot(1,2,2).imshow(psf(30,30), norm=simple_norm(psf(30,30), stretch='log'), cmap='cividis')
+        # pl.title("Fitted model")
+
+
+
+        yy, xx = np.indices([61, 61], dtype=float)
+        grid.x_0 = preg.center.x+30
+        grid.y_0 = preg.center.y+30
+        gpsf2 = grid(xx+preg.center.x, yy+preg.center.y)
+        psf_model = crowdsource.psf.SimplePSF(stamp=gpsf2)
+
+        smoothing_scale = 0.55 # pixels
+        gpsf3 = convolve(gpsf2, Gaussian2DKernel(smoothing_scale))
+        psf_model_blur = crowdsource.psf.SimplePSF(stamp=gpsf3)
+
+        pl.imshow(weight, norm=simple_norm(weight, stretch='log')); pl.colorbar();
+
+
+        results_blur  = fit_im(cutout, psf_model_blur, weight=weight,
+                            nskyx=1, nskyy=1, refit_psf=False, verbose=True)
+        stars, modsky, skymsky, psf = results_blur
+        fits.BinTableHDU(data=stars).writeto(f"{basepath}/{filtername}/{filtername.lower()}_{module}_crowdsource.fits", overwrite=True)
+        fits.PrimaryHDU(data=skymsky, header=im1[1].header).writeto(f"{basepath}/{filtername}/{filtername.lower()}_{module}_crowdsource_skymodel.fits")
+
+
+
+        stars, modsky, skymsky, psf = results_blur
+        pl.figure(figsize=(12,12))
+        pl.subplot(2,2,1).imshow(cutout[:128,:128], norm=simple_norm(cutout[:256,:256], stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("Data")
+        pl.subplot(2,2,2).imshow(modsky[:128,:128], norm=simple_norm(modsky[:256,:256], stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im model+sky")
+        pl.subplot(2,2,3).imshow((cutout-modsky)[:128,:128], norm=simple_norm((cutout-modsky)[:256,:256], stretch='asinh', max_percent=99.5, min_percent=0.5), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("data-modsky")
+        pl.subplot(2,2,4).imshow(cutout[:128,:128], norm=simple_norm(cutout[:256,:256], stretch='log', max_percent=99.95), cmap='gray')
+        pl.subplot(2,2,4).scatter(stars['y'], stars['x'], marker='x', color='r', s=8, linewidth=0.5)
+        pl.axis([0,128,0,128])
+        pl.xticks([]); pl.yticks([]); pl.title("Data with stars");
+        pl.suptitle("Using WebbPSF model blurred a little")
+        pl.tight_layout()
+        pl.savefig(f'{basepath}/{filtername}/pipeline/jw02221-o001_t001_nircam_{pupil}-{filtername.lower()}-{module}_catalog_diagnostics_zoom.png',
+                   bbox_inches='tight')
+
+        pl.figure(figsize=(12,12))
+        pl.subplot(2,2,1).imshow(data, norm=simple_norm(data, stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("Data")
+        pl.subplot(2,2,2).imshow(modsky, norm=simple_norm(modsky, stretch='log', max_percent=99.95), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im model+sky")
+        pl.subplot(2,2,3).imshow(skymsky, norm=simple_norm(skymsky, stretch='asinh'), cmap='gray')
+        pl.xticks([]); pl.yticks([]); pl.title("fit_im sky+skym")
+        pl.subplot(2,2,4).imshow(data, norm=simple_norm(data, stretch='log', max_percent=99.95), cmap='gray')
+        pl.subplot(2,2,4).scatter(stars['y'], stars['x'], marker='x', color='r', s=5, linewidth=0.5)
+        pl.xticks([]); pl.yticks([]); pl.title("Data with stars");
+        pl.savefig(f'{basepath}/{filtername}/pipeline/jw02221-o001_t001_nircam_{pupil}-{filtername.lower()}-{module}_catalog_diagnostics.png',
+                   bbox_inches='tight')
--- reduction/PipelineRerunNIRCAM-SHORT.py
+++ reduction/PipelineRerunNIRCAM-SHORT.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from glob import glob
+from astroquery.mast import Mast, Observations
 import os
 import shutil
 import numpy as np
@@ -23,7 +24,9 @@ from jwst import datamodels
 from jwst.associations import asn_from_list
 from jwst.associations.lib.rules_level3_base import DMS_Level3_Base
 
+import crds
 
+import pprint
 
 import jwst
 print(jwst.__version__)
@@ -227,55 +230,19 @@ def show_image(data_2d, vmin, vmax, xpixel=None, ypixel=None, title=None,
 
 
 
-os.chdir(output_dir)
-
-asn_file = os.path.join(output_dir, 'jw02221-o001_20220828t044842_image3_007_asn.json')
-
-# Open the association file and load into a json object
-with open(asn_file) as f_obj:
-    asn_data = json.load(f_obj)
-    f_obj.seek(0)
-
-tweak_files = ['level3_lw_asn_0_tweakregstep.fits',
-               'level3_lw_asn_1_tweakregstep.fits',
-               'level3_lw_asn_2_tweakregstep.fits']
-tweak_product = 'manual_asn_file'
-
-
-
-tweakreg_asn = asn_from_list.asn_from_list(tweak_files, rule=DMS_Level3_Base, product_name=tweak_product)
-
-
-output_test = 'manual_tweakreg_asn.json'
-with open(output_test, 'w') as outfile:
-    name, serialized = tweakreg_asn.dump(format='json')
-    outfile.write(serialized)
-
-
-
-import crds
-
-
-#rslt = download_file('https://jwst-crds.stsci.edu/unchecked_get/references/jwst/jwst_nircam_pars-tweakregstep_0037.asdf', cache=False)
-#shutil.move(rslt, './jwst_nircam_pars-tweakregstep_0037.asdf')
-
-# tweak_param_reffile = 'jwst_nircam_pars-tweakregstep_0037.asdf'
-
-
-import pprint
-
-
-#with asdf.open(tweak_param_reffile) as tweak_params:
-#    pprint.pprint(tweak_params.tree)
-
-
-
 def main():
+
 
     os.environ["CRDS_PATH"] = "/orange/adamginsburg/jwst/brick/crds/"
     os.environ["CRDS_SERVER_URL"] = "https://jwst-crds-pub.stsci.edu"
     mpl.rcParams['savefig.dpi'] = 80
     mpl.rcParams['figure.dpi'] = 80
+    
+    with open(os.path.expanduser('/home/adamginsburg/.mast_api_token'), 'r') as fh:
+        api_token = fh.read().strip()
+    Mast.login(api_token.strip())
+    Observations.login(api_token)
+
 
     for filtername in ('F187N', 'F182M', 'F212N'):
         # Files created in this notebook will be saved
@@ -283,31 +250,84 @@ def main():
         output_dir = f'/orange/adamginsburg/jwst/brick/{filtername}/pipeline/'
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
+        os.chdir(output_dir)
 
-        for module in ('nrca','nrcb'):
-            for detector in range(1,5):
+        # the files are one directory up
+        for fn in glob("../*cal.fits"):
+            try:
+                os.link(fn, './'+os.path.basename(fn))
+            except Exception as ex:
+                print(f'Failed to link {fn} to {os.path.basename(fn)} because of {ex}')
+
+        Observations.cache_location = output_dir
+        obs_table = Observations.query_criteria(
+                                                #obs_id='jw02221-o001_t001_nircam_clear-f182m',
+                                                filters=filtername.lower(),
+                                                proposal_id="2221",
+                                                proposal_pi="Ginsburg*",
+                                                calib_level=3,
+                                               )
+        print(len(obs_table))
+
+        data_products_by_obs = Observations.get_product_list(obs_table[obs_table['calib_level'] == 3])
+        print(len(data_products_by_obs))
+
+        products_asn = Observations.filter_products(data_products_by_obs, extension="json")
+        print(len(products_asn))
+        valid_obsids = products_asn['obs_id'][np.char.find(np.unique(products_asn['obs_id']), 'jw02221-o001', ) == 0]
+        match = [x for x in valid_obsids if filtername.lower() in x][0]
+
+        asn_mast_data = products_asn[products_asn['obs_id'] == match]
+        print(asn_mast_data)
+
+        manifest = Observations.download_products(asn_mast_data, download_dir=output_dir)
+        print(manifest)
+
+        # MAST creates deep directory structures we don't want
+        for row in manifest:
+            try:
+                shutil.move(row['Local Path'], os.path.join(output_dir, os.path.basename(row['Local Path'])))
+            except Exception as ex:
+                print(f"Failed to move file with error {ex}")
+
+
+        for module in ('nrca', 'nrcb'):
+            for detector in range(1, 5):
+                print(f"Filter {filtername} module {module} detector {detector}")
+                print(f"Searching for {os.path.join(output_dir, f'jw02221-*_image3_0[0-9][0-9]_asn.json')}")
+                asn_file_search = glob(os.path.join(output_dir, f'jw02221-*_image3_0[0-9][0-9]_asn.json'))
                 if len(asn_file_search) == 1:
-                    asn_file_search = glob.glob(os.path.join(output_dir, f'jw02221-*_image3_0[0-9][0-9]_asn.json'))
+                    asn_file = asn_file_search[0]
                 else:
                     raise ValueError("Mismatch")
-                asn_file = asn_file_search[0]
+
+                mapping = crds.rmap.load_mapping('/orange/adamginsburg/jwst/brick/crds/mappings/jwst/jwst_nircam_pars-tweakregstep_0003.rmap')
+                tweakreg_asdf_filename = [x for x in mapping.todict()['selections'] if x[1] == filtername][0][4]
+                tweakreg_asdf = asdf.open(f'https://jwst-crds.stsci.edu/unchecked_get/references/jwst/{tweakreg_asdf_filename}')
+                tweakreg_parameters = tweakreg_asdf.tree['parameters']
+                print(f'Filter {filtername}: {tweakreg_parameters}')
 
                 with open(asn_file) as f_obj:
                     asn_data = json.load(f_obj)
                 asn_data['products'][0]['name'] = f'jw02221-o001_t001_nircam_clear-{filtername.lower()}-{module}{detector}'
                 asn_data['products'][0]['members'] = [row for row in asn_data['products'][0]['members']
                                                       if f'{module}{detector}' in row['expname']]
-                asn_file_each = asn_file_each.replace("_asn.json", "_{module}{detector}_asn.json")
+                asn_file_each = asn_file.replace("_asn.json", f"_{module}{detector}_asn.json")
                 with open(asn_file_each, 'w') as fh:
                     json.dump(asn_data, fh)
-                len(asn_nrca4['products'][0]['members']), len(asn_data['products'][0]['members'])
 
                 image3 = calwebb_image3.Image3Pipeline()
 
                 image3.output_dir = output_dir
                 image3.save_results = True
+                for par in tweakreg_parameters:
+                    setattr(image3.tweakreg, par, tweakreg_parameters[par])
 
                 image3.run(asn_file_each)
                 print(f"DONE running {asn_file_each}")
 
     globals().update(locals())
+    return locals()
+
+if __name__ == "__main__":
+    results = main()
