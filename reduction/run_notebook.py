7199e0171788947fbb5969bb3c4f3242b28918cc
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-09-18 14:44:49 -0400

    add new rerunning machinery

---
+++ reduction/run_notebook.py
@@ -0,0 +1,25 @@
+"""
+https://stackoverflow.com/questions/67811531/how-can-i-execute-a-ipynb-notebook-file-in-a-python-script
+https://nbconvert.readthedocs.io/en/latest/execute_api.html
+"""
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def run_notebook(filename, kernel_name='python39'):
+    print(f"Running notebook {filename}")
+    with open(filename) as ff:
+        nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
+
+    print(f"Writing backup notebook {filename}.backup")
+    with open(filename + ".backup", 'w', encoding='utf-8') as fh:
+        nbformat.write(nb_in, fh)
+
+    ep = ExecutePreprocessor(timeout=600)
+
+    nb_out = ep.preprocess(nb_in)
+
+    print(f"Writing notebook {filename}")
+    with open(filename, 'w', encoding='utf-8') as fh:
+        nbformat.write(nb_in, fh)
+
+    return nb_in, nb_out
