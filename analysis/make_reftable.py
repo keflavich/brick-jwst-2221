092104fbcd6e613e387e28d19f24c8777f33fdf3
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-02-09 16:55:32 -0500

    add some source rejection in the reftable creation

--- analysis/make_reftable.py
+++ analysis/make_reftable.py
@@ -1,4 +1,6 @@
 import datetime
+
+from astropy import units as u
 from astropy.table import Table
 
 def main():
@@ -14,7 +16,14 @@ def main():
     sel = tbl['sep_f466n'].quantity < 0.1*u.arcsec
     sel &= tbl['sep_f405n'].quantity < 0.1*u.arcsec
 
-    reftbl = tbl['skycoord_f410m']
+    # reject sources with bad QFs
+    goodqflong = ((tbl['qf_f410m'] > 0.98) | (tbl['qf_f405n'] > 0.98) | (tbl['qf_f466n'] > 0.98))
+    goodspreadlong = ((tbl['spread_model_f410m'] < 0.025) | (tbl['spread_model_f405n'] < 0.025) | (tbl['spread_model_f466n'] < 0.025))
+    goodfracfluxlong = ((tbl['fracflux_f410m'] > 0.9) | (tbl['fracflux_f405n'] > 0.9) & (tbl['fracflux_f466n'] > 0.9))
+
+    sel &= goodqflong & goodspreadlong & goodfracfluxlong
+
+    reftbl = tbl['skycoord_f410m'][sel]
     reftbl['RA'] = reftbl['skycoord_f410m'].ra
     reftbl['DEC'] = reftbl['skycoord_f410m'].dec
 
