039fafc639c5f573ea4e0d0c36fe4abf55862e71
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-06-11 19:29:01 -0400

    investigating flux cal issue

--- analysis/selections.py
+++ analysis/selections.py
@@ -114,6 +114,7 @@ def main(basetable, ww):
     print(f"Of {len(all_good)} rows, {all_good.sum()} are good in all filters.")
     print(f"Of {len(all_good)} rows, {long_good.sum()} are good in long filters.")
     print(f"Of {len(all_good)} rows, {short_good.sum()} are good in short filters.")
+    print(f"Of {len(all_good)} rows, {any_good.sum()} are good in at least one filter.")
 
     goodqflong = ((basetable['qf_f410m'] > minqf) & (basetable['qf_f405n'] > minqf) & (basetable['qf_f466n'] > minqf))
     goodspreadlong = ((basetable['spread_model_f410m'] < maxspread) | (basetable['spread_model_f405n'] < maxspread) | (basetable['spread_model_f466n'] < maxspread))
@@ -266,6 +267,14 @@ if __name__ == "__main__":
     (options, args) = parser.parse_args()
 
     print(f"Selecting module {options.module}")
+
+    # save nrca and nrcb result tables
+    from analysis_setup import fh_nrca as fh, ww410_nrca as ww410, ww410_nrca as ww
+    result = main(basetable_nrca, ww=ww)
+    globals().update({key+"_a": val for key, val in result.items()})
+    from analysis_setup import fh_nrcb as fh, ww410_nrcb as ww410, ww410_nrcb as ww
+    result = main(basetable_nrcb, ww=ww)
+    globals().update({key+"_b": val for key, val in result.items()})
 
     if options.module == 'nrca':
         from analysis_setup import fh_nrca as fh, ww410_nrca as ww410, ww410_nrca as ww
