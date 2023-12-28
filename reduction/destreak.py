1d36abd2f09b27d28ae1c0742134d4f341ef75aa
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-12-28 14:42:15 -0500

    import scipy in destrek

--- reduction/destreak.py
+++ reduction/destreak.py
@@ -3,6 +3,8 @@ from astropy.io import fits
 from scipy.ndimage import median_filter, map_coordinates
 import numpy as np
 from astropy.wcs import WCS
+import scipy
+import scipy.ndimage
 
 basepath = '/orange/adamginsburg/jwst/brick/'
 
