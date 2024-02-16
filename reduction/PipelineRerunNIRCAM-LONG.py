c9095f8c099b7640c47cc1ab1efb945f5e686f23
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2024-02-16 08:24:28 -0500

    add another diagnostic layer

--- reduction/PipelineRerunNIRCAM-LONG.py
+++ reduction/PipelineRerunNIRCAM-LONG.py
@@ -570,9 +570,14 @@ def check_wcs(fn):
     fa = AsdfInFits.open(fn)
     wcsobj = fa.tree['meta']['wcs']
     print(f"fa['meta']['wcs'] crval={wcsobj.to_fits()[0]['CRVAL1']}, {wcsobj.to_fits()[0]['CRVAL2']}, {wcsobj.forward_transform.param_sets[-1]}")
+    new_1024 = wcsobj.pixel_to_world(1024, 1024)
+    print(f"pixel_to_world(1024,1024) = {new_1024}")
     if 'oldwcs' in fa.tree['meta']:
         oldwcsobj = fa.tree['meta']['oldwcs']
         print(f"fa['meta']['oldwcs'] crval={oldwcsobj.to_fits()[0]['CRVAL1']}, {oldwcsobj.to_fits()[0]['CRVAL2']}, {oldwcsobj.forward_transform.param_sets[-1]}")
+        old_1024 = oldwcsobj.pixel_to_world(1024, 1024)
+        print(f"pixel_to_world(1024,1024) = {old_1024}, sep={old_1024.separation(new_1024)}")
+
 
     # FITS header
     fh = fits.open(fn)
@@ -581,6 +586,9 @@ def check_wcs(fn):
         print(f"OLCRVAL1={fh[1].header['OLCRVAL1']}, OLCRVAL2={fh[1].header['OLCRVAL2']}")
     if 'RAOFFSET' in fh[1].header:
         print("RA, DE offset: ", fh[1].header['RAOFFSET'], fh[1].header['DEOFFSET'])
+    ww = WCS(fh[1].header)
+    fits_1024 = ww.pixel_to_world(1024, 1024)
+    print(f"pixel_to_world(1024,1024) = {fits_1024}, sep={fits_1024.separation(new_1024)}")
 
 if __name__ == "__main__":
     from optparse import OptionParser
