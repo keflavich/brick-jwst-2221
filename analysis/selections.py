80651105e6526a572477a084c989e90c71952ec7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2024-05-11 13:40:35 -0400

    refactor for laptop

--- analysis/selections.py
+++ analysis/selections.py
@@ -300,8 +300,8 @@ def main(basetable, ww):
     # not sure these are legitimately bad?
     # Feb 11, 2023: these are the same objects as 'weird blue'
     # This is needed by some plots, but isn't obviously useful
-    badblue = blue_410m405_466 & ( ((basetable['mag_ab_f405n'] - basetable['mag_ab_f410m']) > 2) 
-                                 # | ((basetable['mag_ab_f410m'] - basetable['mag_ab_f466n']) > -1) 
+    badblue = blue_410m405_466 & ( ((basetable['mag_ab_f405n'] - basetable['mag_ab_f410m']) > 2)
+                                 # | ((basetable['mag_ab_f410m'] - basetable['mag_ab_f466n']) > -1)
                                  )
 
 
@@ -315,25 +315,25 @@ def main(basetable, ww):
 
 
 # def main_dao(basetable, ww):
-# 
+#
 #     # empirical test: these sources are almost certainly saturated in f410m =(
 #     saturated_f410m = ((basetable['mag_ab_f410m'] < (13.9-3.1)) &
 #                        (basetable['mag_ab_f410m'] - basetable['mag_ab_f405n'] >
 #                         0))
 #     basetable['mag_ab_f410m'].mask[saturated_f410m] = True
 #     basetable['flux_f410m'].mask[saturated_f410m] = True
-# 
-# 
+#
+#
 #     # FITS tables can't mask boolean columns
 #     # so, we have to mask the saturated mask using the mask on the flux for the filter
 #     any_saturated_ = [basetable[f'near_saturated_{x}_{x}'] & ~basetable[f'flux_{x}'].mask for x in filternames]
-# 
+#
 #     any_saturated = any_saturated_[0]
 #     for col in any_saturated_[1:]:
 #         print(f"{col.sum()} saturated in {col.name}")
 #         any_saturated = any_saturated | col
 #     print(f"{any_saturated.sum()} near saturated out of {len(basetable)}.  That leaves {(~any_saturated).sum()} not near unsaturated")
-# 
+#
 #     any_replaced_saturated_ = [basetable[f'replaced_saturated_{x}'] &
 #                                ~basetable[f'flux_{x}'].mask for x in filternames]
 #     any_replaced_saturated = any_replaced_saturated_[0]
@@ -341,19 +341,19 @@ def main(basetable, ww):
 #         print(f"{col.sum()} saturated in {col.name}")
 #         any_replaced_saturated = any_replaced_saturated | col
 #     print(f"{any_replaced_saturated.sum()} saturated out of {len(basetable)}.  That leaves {(~any_replaced_saturated).sum()} unsaturated")
-# 
+#
 #     magerr_gtpt1 = np.logical_or.reduce([basetable[f'emag_ab_{filtername}'] > 0.2 for filtername in filternames])
 #     magerr_gtpt1.sum()
-# 
-# 
+#
+#
 #     for filt in filternames:
 #         filt = filt.lower()
 #         mask = basetable[f'mag_ab_{filt}'].mask
-# 
+#
 #         flagok = (basetable[f'flags_{filt}'] == 0)
 #         basetable[f'good_{filt}'] = allok = flagok
 #         print(f"Filter {filt} has {flagok.sum()} flags=0.")
-# 
+#
 #     all_good = np.all([basetable[f'good_{filt}'] for filt in filternames], axis=0)
 #     any_good = np.any([basetable[f'good_{filt}'] for filt in filternames], axis=0)
 #     long_good = np.all([basetable[f'good_{filt}'] for filt in filternames if 'f4' in filt], axis=0)
@@ -362,16 +362,16 @@ def main(basetable, ww):
 #     print(f"Of {len(all_good)} rows, {long_good.sum()} are good in long filters.")
 #     print(f"Of {len(all_good)} rows, {short_good.sum()} are good in short filters.")
 #     print(f"Of {len(all_good)} rows, {any_good.sum()} are good in at least one filter.")
-# 
+#
 #     allgood_long = (basetable['good_f410m'] & basetable['good_f466n'] & basetable['good_f405n'])
-# 
+#
 #     allgood_short = (basetable['good_f212n'] & basetable['good_f187n'] & basetable['good_f182m'])
-# 
+#
 #     # threshold = 0.1 arcsec
 #     oksep = np.logical_and.reduce([basetable[f'sep_{filtername}'] < 0.1*u.arcsec for filtername in filternames[1:]])
 #     print(f"Found {oksep.sum()} of {len(oksep)} sources with separations < 0.1 arcsec")
 #     oklong = oksep & (~any_saturated) & (~(basetable['mag_ab_410m405'].mask))
-# 
+#
 #     jfilts = SvoFps.get_filter_list('JWST')
 #     jfilts.add_index('filterID')
 #     abconv = (1*u.Jy).to(u.ABmag)
@@ -379,7 +379,7 @@ def main(basetable, ww):
 #     filtconv466 = -2.5*np.log10(1/jfilts.loc['JWST/NIRCam.F466N']['ZeroPoint'])-abconv.value
 #     zeropoint_offset_410_466 = filtconv410-filtconv466
 #     print(f'Offset between raw ABmag for F410M-F466N = {filtconv410} - {filtconv466} = {zeropoint_offset_410_466}')
-# 
+#
 #     veryblue_410m405_466 = (oksep & (~any_saturated) & (~(basetable['mag_ab_410m405'].mask)) &
 #                     ((basetable['mag_ab_410m405'] - basetable['mag_ab_f466n']) +
 #                      (basetable['emag_ab_f410m']**2 + basetable['emag_ab_f466n']**2 + basetable['emag_ab_f405n']**2)**0.5 < (-1.75+zeropoint_offset_410_466)
@@ -388,7 +388,7 @@ def main(basetable, ww):
 #                     (((basetable['mag_ab_f410m'] - basetable['mag_ab_f466n']) +
 #                       (basetable['emag_ab_f410m']**2 + basetable['emag_ab_f466n']**2)**0.5) < (-1.75+zeropoint_offset_410_466)
 #                     ))
-# 
+#
 #     blue_410m405_466 = (oksep & (~any_saturated) & (~(basetable['mag_ab_410m405'].mask)) &
 #                     ((basetable['mag_ab_410m405'] - basetable['mag_ab_f466n']) +
 #                      (basetable['emag_ab_f410m']**2 + basetable['emag_ab_f466n']**2 + basetable['emag_ab_f405n']**2)**0.5 < (-0.75+zeropoint_offset_410_466))
@@ -417,7 +417,7 @@ def main(basetable, ww):
 #                      ((basetable['mag_ab_f187n'] - basetable['mag_ab_f182m']) +
 #                       (basetable['emag_ab_f182m']**2 + basetable['emag_ab_f187n']**2)**0.5 < -1)
 #                     & ~magerr_gtpt1)
-# 
+#
 #     blue_BrA_and_PaA = (oksep & ~any_saturated &
 #                         (basetable['flux_f405n'] > basetable['flux_f410m']) &
 #                         (basetable['flux_f187n'] > basetable['flux_f182m']) &
@@ -448,18 +448,18 @@ def main(basetable, ww):
 #     print(f"Strongly blue [410-466] sources: {blue_410_466.sum()}")
 #     print(f"Somewhat blue [410-466] sources: {slightly_blue_410_466.sum()}")
 #     print(oklong.sum(), blue_410_466.sum(), slightly_blue_410_466.sum(), blue_405_410.sum(), blue_405_410b.sum(), blue_BrA_and_PaA.sum(), detected.sum(), blue_BrA_and_PaA.sum() / detected.sum())
-# 
+#
 #     neg_405m410 = basetable['flux_jy_405m410'] < 0
 #     print(f"Negative 405-410 colors: {neg_405m410.sum()}, Nonnegative: {(~neg_405m410).sum()}")
-# 
+#
 #     any_saturated |= saturated_f410m
 #     all_good &= ~saturated_f410m
-# 
+#
 #     exclude = (any_saturated | ~oksep | magerr_gtpt1 |
 #                basetable['mag_ab_f405n'].mask | basetable['mag_ab_f410m'].mask
 #               )
 #     print(f"Excluding {exclude.sum()} of {exclude.size}")
-# 
+#
 #     # "bad" was totally broken; (bad & all_good) is very nonzero
 #     # bad = (any_saturated | ~oksep | magerr_gtpt1 | basetable['mag_ab_f212n'].mask |
 #     #        basetable['mag_ab_f410m'].mask | badqflong | badfracfluxlong |
@@ -468,39 +468,39 @@ def main(basetable, ww):
 #     print("'Bad' sources are those where _any_ filter is masked out")
 #     print(f"Not-bad:{(~bad).sum()}, bad: {bad.sum()},"# bad.mask: {bad.mask.sum()},"
 #           f" len(bad):{len(bad)}, len(table):{len(basetable)}.")
-# 
-# 
+#
+#
 #     # Basic selections for CMD, CCD plotting
 #     sel = reg.contains(basetable['skycoord_f410m'], ww)
 #     sel &= basetable['sep_f466n'].quantity < 0.1*u.arcsec
 #     sel &= basetable['sep_f405n'].quantity < 0.1*u.arcsec
-# 
+#
 #     def ccds_withiso(basetable=basetable, sel=sel, exclude=exclude, **kwargs):
 #         return plot_tools.ccds_withiso(basetable=basetable, sel=sel, exclude=exclude, **kwargs)
-# 
+#
 #     def cmds_withiso(basetable=basetable, sel=sel, exclude=exclude, distance_modulus=distance_modulus, **kwargs):
 #         return plot_tools.cmds_withiso(basetable=basetable, sel=sel, exclude=exclude, distance_modulus=distance_modulus, **kwargs)
-# 
-# 
+#
+#
 #     sel = reg.contains(basetable['skycoord_f410m'], ww)
 #     sel &= basetable['sep_f466n'].quantity < 0.1*u.arcsec
 #     sel &= basetable['sep_f405n'].quantity < 0.1*u.arcsec
-# 
+#
 #     def ccds(basetable=basetable, sel=sel, **kwargs):
 #         return plot_tools.ccds(basetable=basetable, sel=sel, **kwargs)
-# 
+#
 #     def cmds(basetable=basetable, sel=sel, **kwargs):
 #         return plot_tools.cmds(basetable=basetable, sel=sel, **kwargs)
-# 
+#
 #     crds = basetable['skycoord_f410m']
-# 
-# 
+#
+#
 #     # not sure these are legitimately bad?
 #     # Feb 11, 2023: these are the same objects as 'weird blue'
 #     # This is needed by some plots, but isn't obviously useful
 #     badblue = blue_410_466 & ((basetable['mag_ab_f405n'] - basetable['mag_ab_f410m']) > 2)
 #     #| ((basetable['mag_ab_f410m'] - basetable['mag_ab_f466n']) > -0.5) )
-# 
+#
 #     assert 'blue_410m405_466' in locals()
 #     return locals()
 
