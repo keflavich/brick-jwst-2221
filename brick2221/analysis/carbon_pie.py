1ab7069e29cf48026ed36d9470da11209ec57c17
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2026-05-07 15:40:32 -0400

    remove carbon  / C+ from pie charts b/c it should be zero in mol clds

--- brick2221/analysis/carbon_pie.py
+++ brick2221/analysis/carbon_pie.py
@@ -7,12 +7,13 @@ https://chatgpt.com/share/6936e2fc-6c98-8004-a454-78bcb5418235
 import pylab as pl
 
 
-if socket.gethostname() == 'cyg':
+import socket
+if socket.gethostname() in ('cyg', 'cyg2022.local'):
     rootpath = '/Users/adam/Dropbox/talks/talks/ice_assets/'
 elif 'ufhpc' in socket.gethostname():
     rootpath = '/orange/adamginsburg/ice/colors_of_ices_overleaf/figures/'
 else:
-    raise ValueError("Unknown host")
+    raise ValueError(f"Unknown host {socket.gethostname()}")
 
 pl.clf();
 
@@ -26,7 +27,7 @@ pl.savefig(f"{rootpath}/standard_carbon_distribution_exploded.png", bbox_inches=
 pl.clf();
 
 pl.figure(1, figsize=(10, 10), dpi=300)
-pl.pie([0.5, 0.1666, 0.11111, 0.11111, 0.11111], labels=['PAHs & Dust', 'CO gas', 'CO$_2$ ice', 'C/C+', 'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
+pl.pie([0.5, 0.1666, 0.11111, 0.11111], labels=['PAHs & Dust', 'CO gas', 'CO$_2$ ice',  'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
 
 pl.title("Local Carbon:\n C/H$\sim10^{-3.5} = 3\\times10^{-4}$")
 
@@ -36,7 +37,7 @@ pl.savefig(f"{rootpath}/standard_carbon_distribution.png", bbox_inches='tight', 
 pl.clf();
 
 pl.figure(1, figsize=(10, 10), dpi=300)
-pl.pie([0.5, 0.1666, 0.11111, 0.11111, 0.11111], explode=(0,0.1,0,0,0), labels=['PAHs & Dust', 'CO gas / H$_2$ = 1$\\times10^{-4}$', 'CO$_2$ ice', 'C/C+', 'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
+pl.pie([0.5, 0.1666, 0.11111, 0.11111], explode=(0,0.1,0,0), labels=['PAHs & Dust', 'CO gas / H$_2$ = 1$\\times10^{-4}$', 'CO$_2$ ice', 'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
 
 pl.title("Local Carbon:\n C/H$\sim10^{-3.5} = 3\\times10^{-4}$")
 
@@ -45,7 +46,7 @@ pl.savefig(f"{rootpath}/standard_carbon_distribution_exploded_h2.png", bbox_inch
 pl.clf();
 
 pl.figure(1, figsize=(10, 10), dpi=300)
-pl.pie([0.5, 0.1666, 0.11111, 0.11111, 0.11111], explode=(0,0.1,0,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 1$\\times10^{-4}$', 'CO$_2$ ice', 'C/C+', 'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
+pl.pie([0.5, 0.1666, 0.11111, 0.11111], explode=(0,0.1,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 1$\\times10^{-4}$', 'CO$_2$ ice','Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
 
 pl.title("Local Carbon:\n C/H$\sim10^{-3.5} = 3\\times10^{-4}$")
 
@@ -56,7 +57,7 @@ pl.savefig(f"{rootpath}/standard_carbon_distribution_exploded_h2_ice.png", bbox_
 pl.clf();
 
 pl.figure(1, figsize=(10, 10), dpi=300)
-pl.pie([0.35, 0.4166666, 0.0777777, 0.0777777, 0.0777777], explode=(0,0.1,0,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 2.5$\\times10^{-4}$', 'CO$_2$ ice', 'C/C+', 'Other'], labeldistance=0.4, startangle=160, rotatelabels=True, textprops={'fontsize': 8});
+pl.pie([0.35, 0.4166666, 0.0777777, 0.0777777, ], explode=(0,0.1,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 2.5$\\times10^{-4}$', 'CO$_2$ ice',  'Other'], labeldistance=0.4, startangle=160, rotatelabels=True, textprops={'fontsize': 8});
 
 pl.title("Local Carbon:\n C/H$\sim10^{-3.5} = 3\\times10^{-4}$\nwith GC measured CO ice")
 
@@ -70,7 +71,7 @@ pl.clf();
 pl.clf();
 
 pl.figure(1, figsize=(10, 10), dpi=300)
-pl.pie([0.5, 0.1666666, 0.1111111, 0.1111111, 0.1111111], explode=(0,0.1,0,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 2.5$\\times10^{-4}$', 'CO$_2$ ice', 'C/C+', 'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
+pl.pie([0.5, 0.1666666, 0.1111111, 0.1111111], explode=(0,0.1,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 2.5$\\times10^{-4}$', 'CO$_2$ ice',  'Other'], labeldistance=0.4, startangle=90, rotatelabels=True, textprops={'fontsize': 8});
 
 pl.title("Galactic Center Carbon:\n C/H$\sim10^{-3.12} = 7.5\\times10^{-4}$")
 
@@ -120,7 +121,7 @@ pl.savefig(f"{rootpath}/allco_pacman_83pct.png", bbox_inches='tight', dpi=300)
 
 pl.clf()
 pl.figure(1, figsize=(10, 10), dpi=300)
-pl.pie([0.1, 0.8333333, 0.0222222, 0.0222222, 0.0222222], explode=(0,0.1,0,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 5$\\times10^{-4}$', 'CO$_2$ ice', 'C/C+', 'Other'], labeldistance=0.4, startangle=170, rotatelabels=True, textprops={'fontsize': 8});
+pl.pie([0.1, 0.8333333, 0.0222222, 0.0222222], explode=(0,0.1,0,0), labels=['PAHs & Dust', 'CO ice / H$_2$ = 5$\\times10^{-4}$', 'CO$_2$ ice', 'Other'], labeldistance=0.4, startangle=170, rotatelabels=True, textprops={'fontsize': 8});
 
 pl.title("Local Carbon:\n C/H$\sim10^{-3.5} = 3\\times10^{-4}$\nwith GC measured CO ice")
 
